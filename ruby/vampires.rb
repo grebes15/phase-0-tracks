40ec58fb95a3c33eec81dfd0c365126f3cc86614
Andreas Landgrebe <andreaslandgrebe@gmail.com>
2017-07-23 18:59:50 -0400

    add loop to be able to process multiple employees and display the number employee being processed

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,17 +1,26 @@
+#4.5 Solo Challenge: Control Flow Practice
+#Andreas Landgrebe
+
+puts "How many employees will be processed?"
+number_employees_being_processed = gets.chomp.to_i
+
+for x in 1..number_employees_being_processed
+
+puts "Question for Employee #{x}"
 
 puts "What is your name?"
 name_given_response = gets.chomp
+
+drake_cula = "Drake Cula"
+tu_fang = "Tu Fang"
+
+is_vampire = "Results inconclusive"
 
 puts "How old are you? What year were you born?"
 age_response = gets.chomp.to_i
 year_response = gets.chomp.to_i
 
-def convert_year_to_age 
-	year_response = 2017 - year_response
-	if year_response != age_response && garlic_response == "Yes"
-		puts "Probably not a vampire"
-	end
-end
+year_response_to_age = 2017 - year_response
 
 puts "Our company cafeteria servces garlic bread. Should we order some for you?"
 garlic_response = gets.chomp
@@ -19,11 +28,24 @@ garlic_response = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 insurance_response = gets.chomp
 
+if(year_response_to_age == age_response) && (garlic_response == "Yes" || insurance_response == "Yes")
+	is_vampire = "Probably not a vampire"
+end
+if(year_response_to_age != age_response) && (garlic_response == "No" || insurance_response == "No")
+	is_vampire = "Probably a vampire"
+end
+if(year_response_to_age != age_response) && (garlic_response == "No" && insurance_response == "No")
+	is_vampire = "Almost certainly a vampire"
+end
+if (name_given_response == drake_cula || name_given_response == tu_fang)
+	is_vampire = "Definitely a vampire"
+end
+puts is_vampire
+
+end
+
 wolves_like_sunshine = true
 wolves_like_garlic = true
 vampires_like_sunshine = false
 vampires_like_garlic = false
-
-convert_year_to_age()
-
 
