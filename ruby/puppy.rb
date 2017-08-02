f58eaedeb06a8a501e752ff0d8661d657332e649
Andreas Landgrebe <andreaslandgrebe@gmail.com>
2017-08-02 13:24:22 -0400

    add a class design to puppies with characteristics and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,26 @@
+#6.1 Introduction to Classes
+#Andreas Landgrebe  
+
+#CANINES ------------------------
+# Puppies
+#
+#CHARACTERISTICS------------------
+#Eyelash length: varies
+#Friendly: yes
+#Eye count: 2
+#Name: varies
+#Color: varies
+#
+#BEHAVIOR
+#Jump
+#Kiss
+#Fetch
+#Bark
+#Sit
+#Roll Over
+#
+#
+#
+#
+#
+#
