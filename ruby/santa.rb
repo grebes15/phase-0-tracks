ec0c6e09f6ebcbaaaccf885b8228dd7c40afb9e8
Andreas Landgrebe <andreaslandgrebe@gmail.com>
2017-08-04 18:38:20 -0400

    complete 6.3 attributes assignment. Add multiple santas to represent a random age, and a different gender and ethnicity from the example_genders and example_ethnicities arrays

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,13 @@
 class Santa
+
+	attr_reader :ethnicity #getter for ethnicity
+	attr_accessor :age, :gender #getters and seters for age and gender
 	def initialize(gender, ethnicity)
 		puts "Intializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0
+		@age = rand(141)
 	end
 	def speak
 		puts "Ho, ho, ho! Haaappy holidays!"
@@ -18,19 +21,27 @@ class Santa
 	end
 
 	def get_mad_at(name_of_reindeer)
+		@reindeer_ranking= @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name)))
 	end
-
-
-
 end
 
-santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
 
-class_santa = Santa.new("agender", "black")
+class_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
 class_santa.speak
 class_santa.eat_milk_and_cookies("Chocolate-chip")
+p class_santa.age = rand(141)
+p class_santa.gender
+p class_santa.ethnicity
+
+multiple_santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+150.times do |x|
+	multiple_santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+	puts "Here comes Santa Claus. This santa is #{multiple_santas[x].age} years old."
+	puts "This Santa has a gender of #{multiple_santas[x].gender}"
+	puts "This Santa has an ethnicity of #{multiple_santas[x].ethnicity}"
+end
