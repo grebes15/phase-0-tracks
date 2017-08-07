c74167d9242869c100f99353a25b4860faea52fd
Andreas Landgrebe <andreaslandgrebe@gmail.com>
2017-08-07 11:53:49 -0400

    add my reflection porition to my gps

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -29,22 +29,6 @@ def new_list(item_String, quantity = 1)
   	}
   	 $list.push(list_item) 
   	end
-  # list_item = {
-  #   quantity: quantity,
-  #   item: item_String
-  # }
-  #$list = string.split
-  #$list.push(list_item)
-
- # $list.each do |list_item|
-  #   list_item[:quantity] = 1
-  # end
-  # list_item = {
-  #   quantity: quantity,
-  #   item: string
-  # }
-  # $list.push(list_item)
-  # print_list
 end
 
 def add_to_list (item, quantity = 1)
@@ -118,3 +102,13 @@ remove_from_list("Lemonade")
 update_quantity(1, "Ice Cream")
 # puts
 print_list
+
+#Release 4: Reflect
+# I learned that when working on this challenge, having a basic layout of pseudocode before you start programming is very effective.
+# This helps you understand what is going asked and assists you in brainstorming ideas on how to complete a specific assignment.
+# The tradoffs to using an array and hashes for this challenge is that if you use an array of hashes to complete this assignment,
+# one is able to store more items into the list of groceries.
+# A method return the last line in the method unless specified elsewhere with a return statement or a break statement.
+# You can pass several data types into methods as arguments.
+# You can pass information between methods as well as the variables that you are using are already initialized.
+# The concepts that were solidifed in this challenge was being able to organize program effectively by writing pseudocode when it is appropriate.
