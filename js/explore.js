92229839d229fa2447585ff8de2ae4fed12e4a62
Andreas Landgrebe <andreaslandgrebe@gmail.com>
2017-08-11 17:34:03 -0400

    add explore.js. add function to reverse a string in java

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+
+
+function reverse(input_string) {
+	var reversed_string = "";
+	for (var x = 0; x < input_string.length; x++) {
+		reversed_string += input_string[x];
+	}
+	return reversed_string;
+}
+
